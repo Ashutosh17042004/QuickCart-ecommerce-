767c96bf28fdb5e578c2cb8b086fc408204a1ffd
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-01 22:42:26 +0530

    feat: Add seller dashboard components and product management features
    - Created AddProduct component for adding new products with image upload functionality.
    - Implemented ProductList component to display all products with loading state.
    - Developed Banner, FeaturedProduct, and HomeProducts components for showcasing products.
    - Added Footer and Navbar components for consistent layout across pages.
    - Introduced Loading component for better user experience during data fetching.
    - Created OrderSummary component to summarize order details and handle address selection.
    - Implemented ProductCard component for displaying individual product details.
    - Added seller-specific components: Footer, Navbar, and Sidebar for navigation.
    - Established AppContext for global state management including cart and user data.

---
+++ components/Navbar.jsx
@@ -0,0 +1,250 @@
+"use client";
+import React from "react";
+import { assets, BagIcon, BoxIcon, CartIcon, HomeIcon } from "@/assets/assets";
+import Link from "next/link";
+import { useAppContext } from "@/context/AppContext";
+import Image from "next/image";
+import { useClerk, UserButton } from "@clerk/nextjs";
+
+const Navbar = () => {
+  const { isSeller, router, user } = useAppContext();
+  const { openSignIn } = useClerk();
+
+  return (
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
+      <Image
+        className="cursor-pointer w-28 md:w-32"
+        onClick={() => router.push("/")}
+        src={assets.logo}
+        alt="logo"
+      />
+      <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
+        <Link href="/" className="hover:text-gray-900 transition">
+          Home
+        </Link>
+        <Link href="/all-products" className="hover:text-gray-900 transition">
+          Shop
+        </Link>
+        <Link href="/" className="hover:text-gray-900 transition">
+          About Us
+        </Link>
+        <Link href="/" className="hover:text-gray-900 transition">
+          Contact
+        </Link>
+
+        {isSeller && (
+          <button
+            onClick={() => router.push("/seller")}
+            className="text-xs border px-4 py-1.5 rounded-full"
+          >
+            Seller Dashboard
+          </button>
+        )}
+      </div>
+
+      <ul className="hidden md:flex items-center gap-4 ">
+        <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
+        {user ? (
+          <>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Cart"
+                  labelIcon={<CartIcon />}
+                  onClick={() => router.push("/cart")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="My Orders"
+                  labelIcon={<BagIcon />}
+                  onClick={() => router.push("/my-orders")}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
+      </ul>
+
+      <div className="flex items-center md:hidden gap-3">
+        {isSeller && (
+          <button
+            onClick={() => router.push("/seller")}
+            className="text-xs border px-4 py-1.5 rounded-full"
+          >
+            Seller Dashboard
+          </button>
+        )}
+          {user ? (
+          <>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Home"
+                  labelIcon={<HomeIcon />}
+                  onClick={() => router.push("/")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Product"
+                  labelIcon={<BoxIcon />}
+                  onClick={() => router.push("/all-products")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="Cart"
+                  labelIcon={<CartIcon />}
+                  onClick={() => router.push("/cart")}
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label="My Orders"
+                  labelIcon={<BagIcon />}
+                  onClick={() => router.push("/my-orders")}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+// "use client";
+
+// import React from "react";
+// import { assets } from "@/assets/assets";
+// import Link from "next/link";
+// import { useAppContext } from "@/context/AppContext";
+// import Image from "next/image";
+// import { useClerk, useUser, UserButton } from "@clerk/nextjs";
+
+// const Navbar = () => {
+//   const { isSeller, router } = useAppContext();
+//   const { openSignIn } = useClerk();
+//   const { isSignedIn } = useUser();
+
+//   return (
+//     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
+//       {/* Logo */}
+//       <Image
+//         className="cursor-pointer w-28 md:w-32 h-auto"
+//         onClick={() => router.push("/")}
+//         src={assets.logo}
+//         alt="logo"
+//         width={140}
+//         height={40}
+//         priority
+//       />
+
+//       {/* Desktop Navigation */}
+//       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
+//         <Link href="/" className="hover:text-gray-900 transition">
+//           Home
+//         </Link>
+
+//         <Link href="/all-products" className="hover:text-gray-900 transition">
+//           Shop
+//         </Link>
+
+//         <Link href="/" className="hover:text-gray-900 transition">
+//           About Us
+//         </Link>
+
+//         <Link href="/" className="hover:text-gray-900 transition">
+//           Contact
+//         </Link>
+
+//         {isSeller && (
+//           <button
+//             onClick={() => router.push("/seller")}
+//             className="text-xs border px-4 py-1.5 rounded-full"
+//           >
+//             Seller Dashboard
+//           </button>
+//         )}
+//       </div>
+
+//       {/* Desktop Right Side */}
+//       <ul className="hidden md:flex items-center gap-4">
+//         <Image
+//           className="w-4 h-4"
+//           src={assets.search_icon}
+//           alt="search icon"
+//           width={16}
+//           height={16}
+//         />
+
+//         {isSignedIn ? (
+//           <UserButton afterSignOutUrl="/" />
+//         ) : (
+//           <button
+//             onClick={() => openSignIn()}
+//             className="flex items-center gap-2 hover:text-gray-900 transition"
+//           >
+//             <Image
+//               src={assets.user_icon}
+//               alt="user icon"
+//               width={20}
+//               height={20}
+//             />
+//             Account
+//           </button>
+//         )}
+//       </ul>
+
+//       {/* Mobile Right Side */}
+//       <div className="flex items-center md:hidden gap-3">
+//         {isSeller && (
+//           <button
+//             onClick={() => router.push("/seller")}
+//             className="text-xs border px-4 py-1.5 rounded-full"
+//           >
+//             Seller Dashboard
+//           </button>
+//         )}
+
+//         {isSignedIn ? (
+//           <UserButton afterSignOutUrl="/" />
+//         ) : (
+//           <button
+//             onClick={() => openSignIn()}
+//             className="flex items-center gap-2 hover:text-gray-900 transition"
+//           >
+//             <Image
+//               src={assets.user_icon}
+//               alt="user icon"
+//               width={20}
+//               height={20}
+//             />
+//             Account
+//           </button>
+//         )}
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
