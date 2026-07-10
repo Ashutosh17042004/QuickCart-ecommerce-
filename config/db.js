dc226655cdb48924311b346e22a3893f3edb1c60
Ashutosh17042004 <ashutoshkumarsinghh17a@gmail.com>
2026-07-10 12:11:53 +0530

    Fix mongodb connection

--- config/db.js
+++ config/db.js
@@ -1,24 +1,19 @@
 import mongoose from "mongoose";
+
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    const opts = {
-      buffercommands: false,
-    };
-    cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
-      .then((mongoose) => {
-        return mongoose;
-      });
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`);
   }
 
   cached.conn = await cached.promise;
